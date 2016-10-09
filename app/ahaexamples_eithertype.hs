c3cbd401310ea04b0ab2034ccf62f4c6fd776849
karan-ta <karan@kodeplay.com>
2016-10-09 20:50:52 +0530

    rename

--- app/ahaexamples_eithertype.hs
+++ app/ahaexamples_eithertype.hs
@@ -1,3 +1,16 @@
 --ahaexamples_eithertype.hs
--- write a type that fails and also gives a failure message ??
-data Result a b = Left a |Right b
+data DivArgs = MakeArgs {
+    numerator :: Float,
+    denominator ::Float
+}
+
+data Myresult a b = Lefty a | Righty b deriving Show
+division :: DivArgs -> Myresult String Float
+division (MakeArgs numerator denominator)
+    | denominator == 0 = Lefty ("division by 0 not allowed")
+    | otherwise = Righty (numerator / denominator)
+
+main :: IO ()
+main = do
+	putStrLn $ show $ division (MakeArgs {numerator=2,denominator=1})
+	putStrLn $ show $ (Lefty "karan" :: Myresult String Int)
